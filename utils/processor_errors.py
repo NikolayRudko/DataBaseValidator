45f71f34e06144f678cabcaaa2aa18ff170c1457
NikolayRudko <niklaus.rud@gmail.com>
2022-03-01 20:09:55 +0300

    Fix: processor_errors.py.

--- utils/processor_errors.py
+++ utils/processor_errors.py
@@ -1,16 +1,5 @@
 class ProcessorError(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
-
-    def __str__(self):
-        print('calling str')
-        if self.message:
-            return '{}, {} '.format(self.__name__, self.message)
-        else:
-            return '{} has been raised'.format(self.__name__)
+    pass
 
 
 class DataTypeProcessorError(ProcessorError):
